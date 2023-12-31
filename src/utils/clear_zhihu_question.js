0b5c8cc0b5e48ff97e6e34bbec6b74ac91f1d661
weisiwu <2436887475@qq.com>
2023-12-31 11:14:59 +0800

    feat: 清空知乎关注问题

--- src/utils/clear_zhihu_question.js
+++ src/utils/clear_zhihu_question.js
@@ -1,47 +1,41 @@
-import axios from "axios";
+import axios from 'axios';
 
-const pageParams = { offset: 0, limit: 20, total: 0 };
-const listApi = (offset) =>
-  `https://www.zhihu.com/api/v4/members/wei-si-wu/following-questions?offset=${offset}&limit=${pageParams.limit}`;
+const pageParams = { offset: 120, limit: 20, total: 0 };
+const listApi = (offset) => `https://www.zhihu.com/api/v4/members/wei-si-wu/following-questions?offset=${offset}&limit=${pageParams.limit}`;
 // DELETE 方法
-const cancelApi = (question_id) =>
-  `https://www.zhihu.com/api/v4/questions/${question_id}/followers`;
+const cancelApi = (question_id) => `https://www.zhihu.com/api/v4/questions/${question_id}/followers`;
 
 const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
 
-const cancel_follow_question = (question_id) =>
-  axios.delete(cancelApi(question_id));
+const cancel_follow_question = (question_id) => axios.delete(cancelApi(question_id));
 
-const clear_zhihu_follow_question = () =>
-  axios.get(listApi(pageParams.offset)).then((result) => {
-    const { status, data } = result || {};
-    console.log("wswTeststatus", status);
-    if (status !== 200) {
-      return false;
-    }
-    const { data: list, paging } = data || [];
-    console.log("wswTest: pageParams.total", pageParams.total);
-    if (!pageParams.total) {
-      pageParams.total = paging.totals;
-    }
-    if (pageParams.offset && pageParams.total <= pageParams.offset) {
-      return false;
-    }
-    if (list?.length) {
-      console.log("wswTestpageParams.offset: ", pageParams.offset);
-      pageParams.offset += list?.length || pageParams.limit;
-      list
-        .reduce((sum, question) => {
-          console.log("wswTestquestion: ", question);
-          if (question.id) {
-            return sum.then(() =>
-              sleep(500).then(() => cancel_follow_question(question.id)),
-            );
-          }
-          return sum.then(() => sleep(500));
-        }, Promise.resolve())
-        .then(() => clear_zhihu_follow_question());
-    }
-  });
+const clear_zhihu_follow_question = () => axios.get(listApi(pageParams.offset)).then((result) => {
+  const { status, data } = result || {};
+  console.log('wswTeststatus', status);
+  if (status !== 200) {
+    return false;
+  }
+  const { data: list, paging } = data || [];
+  console.log('wswTest: pageParams.total', pageParams.total);
+  if (!pageParams.total) {
+    pageParams.total = paging.totals;
+  }
+  if (pageParams.offset && pageParams.total <= pageParams.offset) {
+    return false;
+  }
+  if (list?.length) {
+    console.log('wswTestpageParams.offset: ', pageParams.offset);
+    pageParams.offset += list?.length || pageParams.limit;
+    list
+      .reduce((sum, question) => {
+        console.log('wswTestquestion: ', question);
+        if (question.id) {
+          return sum.then(() => sleep(500).then(() => cancel_follow_question(question.id)));
+        }
+        return sum.then(() => sleep(500));
+      }, Promise.resolve())
+      .then(() => clear_zhihu_follow_question());
+  }
+});
 
 export default clear_zhihu_follow_question;
